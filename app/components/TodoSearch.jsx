13bd02e1051cfeca8f4905be8514b37e83a58eca
chrisrymer <rymercho@gmail.com>
2016-10-29 20:49:03 +0100

    Add search component

---
+++ app/components/TodoSearch.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+
+var AddTodo = React.createClass({
+    handleSubmit: function (e) {
+        e.preventDefault();
+
+        var todoText = this.refs.todoText.value;
+
+        if(todoText.length > 0) {
+            this.refs.todoText.value = '';
+            this.props.onAddTodo(todoText);
+        }
+    },
+   render: function () {
+       return (
+           <div>
+               <form onSubmit={this.handleSubmit}>
+                   <div>
+                       <input type="text" ref="todoText" placeholder="What do you need to do?"/>
+                   </div>
+                   <div>
+                       <button className="button hollow expanded">Add Todo</button>
+                   </div>
+               </form>
+           </div>
+       );
+   }
+});
+
+module.exports = AddTodo;
